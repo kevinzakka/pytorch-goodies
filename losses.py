877eb610c2fc49f26ff1dab93b52306a92b9d284
Kevin <kevinarmandzakka@gmail.com>
2018-09-18 09:48:46 -0700

    fix for single element batch case

--- losses.py
+++ losses.py
@@ -63,14 +63,14 @@ def dice_loss(true, logits, eps=1e-7):
         true: a tensor of shape [B, 1, H, W].
         logits: a tensor of shape [B, C, H, W]. Corresponds to
             the raw output or logits of the model.
-        eps:
+        eps: added to the denominator for numerical stability.
 
     Returns:
         dice_loss: the Sørensen–Dice loss.
     """
     num_classes = logits.shape[1]
     if num_classes == 1:
-        true_1_hot = torch.eye(num_classes + 1)[true.squeeze()]
+        true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
         true_1_hot_f = true_1_hot[:, 0:1, :, :]
         true_1_hot_s = true_1_hot[:, 1:2, :, :]
@@ -79,7 +79,7 @@ def dice_loss(true, logits, eps=1e-7):
         neg_prob = 1 - pos_prob
         probas = torch.cat([pos_prob, neg_prob], dim=1)
     else:
-        true_1_hot = torch.eye(num_classes)[true.squeeze()]
+        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
         probas = F.softmax(probas, dim=1)
     dims = (0,) + tuple(range(2, true.ndimension()))
@@ -93,21 +93,21 @@ def jaccard_loss(true, pred, eps=1e-7):
     """Computes the Jaccard loss, a.k.a the IoU loss.
 
     Note that PyTorch optimizers minimize a loss. In this
-    case, we would like to maximize the dice loss so we
-    return the negated dice loss.
+    case, we would like to maximize the jaccard loss so we
+    return the negated jaccard loss.
 
     Args:
         true: a tensor of shape [B, H, W] or [B, 1, H, W].
         logits: a tensor of shape [B, C, H, W]. Corresponds to
             the raw output or logits of the model.
-        eps:
+        eps: added to the denominator for numerical stability.
 
     Returns:
         jacc_loss: the Jaccard loss.
     """
     num_classes = logits.shape[1]
     if num_classes == 1:
-        true_1_hot = torch.eye(num_classes + 1)[true.squeeze()]
+        true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
         true_1_hot_f = true_1_hot[:, 0:1, :, :]
         true_1_hot_s = true_1_hot[:, 1:2, :, :]
@@ -116,7 +116,7 @@ def jaccard_loss(true, pred, eps=1e-7):
         neg_prob = 1 - pos_prob
         probas = torch.cat([pos_prob, neg_prob], dim=1)
     else:
-        true_1_hot = torch.eye(num_classes)[true.squeeze()]
+        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
         probas = F.softmax(probas, dim=1)
     dims = (0,) + tuple(range(2, true.ndimension()))
