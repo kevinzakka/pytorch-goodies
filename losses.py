46bb5082e7ba63934f7d81b2792f4854c4e8d871
mfmezger <57255687+mfmezger@users.noreply.github.com>
2019-11-22 13:33:59 +0100

    Update losses.py
    Changed probas to logits.

--- losses.py
+++ losses.py
@@ -118,7 +118,7 @@ def jaccard_loss(true, logits, eps=1e-7):
     else:
         true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-        probas = F.softmax(probas, dim=1)
+        probas = F.softmax(logits, dim=1)
     true_1_hot = true_1_hot.type(logits.type())
     dims = (0,) + tuple(range(2, true.ndimension()))
     intersection = torch.sum(probas * true_1_hot, dims)
@@ -163,7 +163,7 @@ def tversky_loss(true, logits, alpha, beta, eps=1e-7):
     else:
         true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
         true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-        probas = F.softmax(probas, dim=1)
+        probas = F.softmax(logits, dim=1)
     true_1_hot = true_1_hot.type(logits.type())
     dims = (0,) + tuple(range(2, true.ndimension()))
     intersection = torch.sum(probas * true_1_hot, dims)
